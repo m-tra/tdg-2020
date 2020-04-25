d8d71421411a5ef549c65bb29b1453680b91e060
marct <marct@LAPTOP-B4C5487U.home>
2020-04-25 23:09:52 +0200

    Circuit Fonctionneel

--- src/main/java/tga_algo/Main.java
+++ src/main/java/tga_algo/Main.java
@@ -6,24 +6,33 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String ngraphe;
-        Graphe actuel;
-        Builder ecrit;
+        String exit = "1";
         do {
-            System.out.println("Choisissez le graphe à traiter (entier entre 1 et 13)");
+            String ngraphe;
+            Graphe actuel;
+            Builder ecrit;
+            System.out.println("Entrez un caractère pour continuer et tapez 0 pour quitter");
             ngraphe = in.nextLine();
-            if (Integer.parseInt(ngraphe) > 13 || Integer.parseInt(ngraphe) < 1) {
-                System.out.println("Choisissez un nombre entre 1 et 13");
+            if (!ngraphe.equals(0)) {
+                do {
+                    System.out.println("Choisissez le graphe à traiter (entier entre 1 et 13)");
+                    ngraphe = in.nextLine();
+                    if (Integer.parseInt(ngraphe) > 13 || Integer.parseInt(ngraphe) < 1) {
+                        System.out.println("Choisissez un nombre entre 1 et 13");
+                    }
+                } while (Integer.parseInt(ngraphe) > 13 || Integer.parseInt(ngraphe) < 1);
+                Chargement nvgraphe = new Chargement(ngraphe);
+                actuel = nvgraphe.getGraphelu();
+                ecrit = new Builder(actuel, ngraphe);
+                ecrit.carac();
+                ecrit.arcs();
+                System.out.println("Ecriture du tableau 2D dans le fichier");
+                ecrit.matrices();
+                ecrit.Circuit();
+
             }
-        } while (Integer.parseInt(ngraphe) > 13 || Integer.parseInt(ngraphe) < 1);
-        Chargement nvgraphe = new Chargement(ngraphe);
-        actuel = nvgraphe.getGraphelu();
-        ecrit = new Builder(actuel, ngraphe);
-        ecrit.carac();
-        ecrit.arcs();
-        System.out.println("Ecriture du tableau 2D dans le fichier");
-        ecrit.matrices();
-        ecrit.Circuit();
+        } while (exit.equals(0));
         System.exit(0);
     }
+
 }
