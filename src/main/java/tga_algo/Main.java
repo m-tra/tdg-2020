f8d0c041dcdb90a82b8b8af8b2a5eb0f74758b62
marct <marct@LAPTOP-B4C5487U.home>
2020-04-26 16:39:02 +0200

    Projet complet(sauf 6 faut pas forcer)

--- src/main/java/tga_algo/Main.java
+++ src/main/java/tga_algo/Main.java
@@ -12,8 +12,9 @@ public class Main {
             Graphe actuel;
             Builder ecrit;
             System.out.println("Entrez un caractère pour continuer et tapez 0 pour quitter et tapez -1 pour exécuter le programme pour tous les graphes");
+            System.out.println("Tapez -2 pour avoir toutes les traces d'éxecution dans le même fichier");
             ngraphe = in.nextLine();
-            if (!ngraphe.equals("0") && !ngraphe.equals("-1")) {
+            if (!ngraphe.equals("0") && !ngraphe.equals("-1")&&!ngraphe.equals("-2")) {
                 do {
                     System.out.println("Choisissez le graphe à traiter (entier entre 1 et 13)");
                     ngraphe = in.nextLine();
@@ -47,7 +48,22 @@ public class Main {
                     ecrit.calculRang();
                     ecrit.Ordonnacement();
                 }
-            } else {
+            }else if (ngraphe.equals("-2")){
+                for (int i = 1; i < 14; i++) {
+                    Chargement nvgraphe = new Chargement(String.valueOf(i));
+                    actuel = nvgraphe.getGraphelu();
+                    ecrit = new Builder(actuel, " complet");
+                    ecrit.carac();
+                    ecrit.arcs();
+                    System.out.println("Ecriture du tableau 2D dans le fichier");
+                    ecrit.matrices();
+                    ecrit.Circuit();
+                    System.out.println("\n");
+                    ecrit.calculRang();
+                    ecrit.Ordonnacement();
+                }
+            } 
+            else {
                 System.exit(0);
             }
         } while (!exit.equals("0"));
